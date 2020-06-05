617e09de8c832e12d9c128b3f15b57b3fe3d707d
Gaëtan GROND <gaetan.grond@gmail.com>
2020-06-05 18:05:51 +0200

    test entry database

---
+++ config.py
@@ -0,0 +1,3 @@
+"""Config for the database and SqlAlchemy."""
+from models import Product, Category, Store
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,13 @@
-"""Create database and insert data from the API."""
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///test.db', echo=True)
+engine = create_engine('sqlite:///:memory:', echo=True)
 Session = sessionmaker(bind=engine)
+Session = sessionmaker()
+session = Session()
 Base = declarative_base()
 Base.metadata.create_all(engine)
-
-session = Session()
 
 
 class Product(Base):
@@ -21,8 +18,9 @@ class Product(Base):
     url = Column(String)
 
     def __repr__git(self):
-        """Render Product object in a readable way"""
+        """Render Product object in a readable way."""
         return f"{product_name} {nutriscore_grade} {url}"
+
 
 class Store(Base):
     __tablename__ = 'store'
@@ -30,7 +28,7 @@ class Store(Base):
     store_name = Column(String(80), unique=True)
 
     def __repr__git(self):
-        """Render Store object in a readable way"""
+        """Render Store object in a readable way."""
         return f"{store_name}"
 
 
@@ -38,12 +36,13 @@ class Category(Base):
     __tablename__ = 'category'
     category_id = Column(Integer, primary_key=True)
     category_name = Column(String(100), unique=True)
-    
+
     def __repr__git(self):
-        """Render Category object in a readable way"""
+        """Render Category object in a readable way."""
         return f"{category_name}"
 
 
-
-for instance in session.query(Category):
-    print(instance.category_store)
+Session.configure(bind=engine)
+test_cat = Category(category_name='Jus de fruits')
+session.add(test_cat)
+session.commit()
