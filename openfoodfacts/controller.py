1ff1d89cd7e52137377e41e5a51c13cb8762468f
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-10-05 16:44:41 +0200

    Fix and comments

--- openfoodfacts/controller.py
+++ openfoodfacts/controller.py
@@ -1,9 +1,11 @@
 """Display a menu for the user to choose a better product."""
 from colorama import init
 from colorama import Fore, Back, Style
-init()
+
 from .managers import session, Store, Category, Product
 from pprint import pprint
+
+init()
 
 class UserMenu:
     """Handle the logic to display the menu
@@ -21,18 +23,20 @@ class UserMenu:
 
         print(Fore.GREEN + "\n------ Menu principal ------\n")
         print(Fore.YELLOW + "1 - Choisir un substitut\n2 - Mes favoris")
-        choice = input("Choissisez une option:")
+        choice = input("Choissisez une option:") #valider differents choix
         if choice == "1":
-            print("hello")
+            print("hello") #return methode qui va bien, menu categorie, return self cateogry menu
+            # def favoris menu
         else:
-            return getattr(self, choice)
+            return getattr(self, choice) # return self main menu on reaffiche le menu encore et encore
 
 
     def category_menu(self):
         """Display categories for the user """
-
+        # afficher categories depuis base avec les chiffres jusqua 5
         print(Fore.GREEN + "\n------ Catégories ------\n")
-        choice = input("Choissisez une catégorie:")
+        choice = input("Choissisez une catégorie:") # tant que pas de categorie continuer donc return categorie menu
+
         return getattr(self, choice)
 
     def product_menu(self):
